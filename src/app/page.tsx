439c9cfd42a2aafaece93c2ffd29db78de7ab74e
bran00 <86134682+Bran00@users.noreply.github.com>
2024-01-22 12:01:36 -0400

    feat: structure to ordinate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,47 @@
-'use client'
-import React from 'react';
-import styles from './page.module.css';
-import ProductCard from '../components/ProductCard';
-import { useProductContext } from '../contexts/ProductContext';
+"use client"
+import React, { useEffect, useState } from "react"
+import styles from "./page.module.css"
+import ProductCard from "../components/ProductCard"
+import { products } from "../constants"
 
 export default function Home() {
-  const { products } = useProductContext();
+  const [isClient, setIsClient] = useState(false)
+  const [sortedProducts, setSortedProducts] = useState([...products])
+  const [sortBy, setSortBy] = useState<"price" | "date">("price")
 
-  return (
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortBy(event.target.value as "price" | "date")
+  }
+
+  useEffect(() => {
+    const sorted = [...products].sort((a, b) => {
+      if (sortBy === "price") {
+        return a.price - b.price
+      } else {
+        return new Date(a.date).getTime() - new Date(b.date).getTime()
+      }
+    })
+    setSortedProducts(sorted)
+  }, [sortBy])
+
+  return isClient ? (
     <main className={styles.main}>
-      {products.map((product) => (
+      <div>
+        <label htmlFor="sort">Ordenar por:</label>
+        <select id="sort" value={sortBy} onChange={handleSortChange}>
+          <option value="price">Preço</option>
+          <option value="date">Data de Inclusão</option>
+        </select>
+      </div>
+      {sortedProducts.map((product) => (
         <ProductCard key={product.id} {...product} />
       ))}
     </main>
-  );
+  ) : (
+    <p>Carregando</p>
+  )
 }
