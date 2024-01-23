37675c7f5b0d4441bed08ec31c50a77105bb0bbc
bran00 <86134682+Bran00@users.noreply.github.com>
2024-01-23 10:02:16 -0400

    feat: imcludes image to assets and constants

---
+++ src/assets/index.ts
@@ -0,0 +1,19 @@
+import bcf1 from "./images/bcf1.jpg"
+import bcf2 from "./images/bcf2.jpg"
+import bcf3 from "./images/bcf3.jpg"
+import bcf4 from "./images/bcf4.jpg"
+import bcf5 from "./images/bcf5.jpg"
+import bcf6 from "./images/bcf6.jpg"
+import bcf7 from "./images/bcf7.jpg"
+import bcf8 from "./images/bcf8.jpg"
+
+export {
+  bcf1,
+  bcf2,
+  bcf3,
+  bcf4,
+  bcf5,
+  bcf6,
+  bcf7,
+  bcf8
+}
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,13 @@
+import { bcf1, bcf2, bcf3, bcf4, bcf5, bcf6, bcf7, bcf8 } from "../assets"
+
 interface Product {
   id: number
   name: string
   price: number
-  date: Date // Adicionado o campo date como opcional
+  date?: Date
+  img?: any
 }
 
-// Função para gerar uma data aleatória dentro de um intervalo
 const randomDate = (start: Date, end: Date): Date => {
   return new Date(
     start.getTime() + Math.random() * (end.getTime() - start.getTime())
@@ -15,50 +17,58 @@ const randomDate = (start: Date, end: Date): Date => {
 export const products: Product[] = [
   {
     id: 1,
-    name: "Semente de Soja A",
+    name: "Semente de Soja A2lO",
     price: 20,
     date: randomDate(new Date(2022, 0, 1), new Date()),
+    img: bcf1,
   },
   {
     id: 2,
-    name: "Semente de Soja B",
+    name: "Semente de Soja BF43",
     price: 30,
     date: randomDate(new Date(2022, 0, 1), new Date()),
+    img: bcf2,
   },
   {
     id: 3,
-    name: "Semente de Soja C",
+    name: "Semente de Soja CR32",
     price: 25,
     date: randomDate(new Date(2022, 0, 1), new Date()),
+    img: bcf3,
   },
   {
     id: 4,
-    name: "Semente de Soja D",
+    name: "Semente de Soja D98P",
     price: 85,
     date: randomDate(new Date(2022, 0, 1), new Date()),
+    img: bcf4,
   },
   {
     id: 5,
-    name: "Semente de Soja E",
+    name: "Semente de Soja ERT5",
     price: 42,
     date: randomDate(new Date(2022, 0, 1), new Date()),
+    img: bcf5,
   },
   {
     id: 6,
-    name: "Semente de Soja F",
+    name: "Semente de Soja F58G",
     price: 14,
     date: randomDate(new Date(2022, 0, 1), new Date()),
+    img: bcf6,
   },
   {
     id: 7,
-    name: "Semente de Soja G",
+    name: "Semente de Soja G98W",
     price: 98,
     date: randomDate(new Date(2022, 0, 1), new Date()),
+    img: bcf7,
   },
   {
     id: 8,
-    name: "Semente de Soja H",
+    name: "Semente de Soja H78A",
     price: 62,
     date: randomDate(new Date(2022, 0, 1), new Date()),
+    img: bcf8,
   },
 ]
